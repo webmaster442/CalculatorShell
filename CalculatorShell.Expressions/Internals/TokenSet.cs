50eb3b0a25cb9c19626f4b706f444fc7753dbd0e
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-06-16 18:34:05 +0200

    Token Types

---
+++ CalculatorShell.Expressions/Internals/TokenSet.cs
@@ -0,0 +1,41 @@
+﻿namespace CalculatorShell.Expressions.Internals
+{
+    internal struct TokenSet
+    {
+        private readonly uint _tokens;
+
+        public TokenSet(params TokenType[] tokens)
+        {
+            _tokens = 0;
+            foreach (var token in tokens)
+            {
+                _tokens |= (uint)token;
+            }
+        }
+
+        public TokenSet(TokenSet set)
+        {
+            _tokens = set._tokens;
+        }
+
+        private TokenSet(uint tokens)
+        {
+            _tokens = tokens;
+        }
+
+        public static TokenSet operator +(TokenSet t1, TokenSet t2)
+        {
+            return new TokenSet(t1._tokens | t2._tokens);
+        }
+
+        public static TokenSet operator +(TokenSet t1, TokenType t2)
+        {
+            return new TokenSet(t1._tokens | (uint)t2);
+        }
+
+        public bool Contains(TokenType type)
+        {
+            return (_tokens & (uint)type) != 0;
+        }
+    }
+}
---
+++ CalculatorShell.Expressions/Internals/TokenType.cs
@@ -0,0 +1,25 @@
+﻿namespace CalculatorShell.Expressions.Internals
+{
+    internal enum TokenType : uint
+    {
+        None = 0x0,
+        Constant = 0x1,
+        Variable = 0x2,
+        Not = 0x4,
+        Plus = 0x8,
+        Minus = 0x10,
+        Multiply = 0x20,
+        Divide = 0x40,
+        And = 0x80,
+        Or = 0x100,
+        Exponent = 0x200,
+        Function1 = 0x400,
+        Function2 = 0x800,
+        ArgumentDivider = 0x1000,
+
+        OpenParen = 0x2000,
+        CloseParen = 0x4000,
+
+        Eof = 0x10000
+    }
+}
