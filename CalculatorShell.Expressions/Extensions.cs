dd9a6e00fc0e837c51cffcb066713c45c376260a
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-07-07 20:51:30 +0200

    Extension to get minterms

--- CalculatorShell.Expressions/Extensions.cs
+++ CalculatorShell.Expressions/Extensions.cs
@@ -1,5 +1,6 @@
 ﻿using CalculatorShell.Expressions.Internals;
 using CalculatorShell.Expressions.Internals.Expressions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -62,9 +63,55 @@ namespace CalculatorShell.Expressions
             return expression is And
                 || expression is Or
                 || expression is Not
-                || expression is Constant
+                || (expression is Constant constant && constant.Value.NumberType == NumberType.Boolean)
                 || expression is Variable;
         }
 
+        /// <summary>
+        /// Create a list of minterms from the given expression
+        /// </summary>
+        /// <param name="expression">Expression to check</param>
+        /// <returns>Minterms of the expression</returns>
+        public static IEnumerable<int> GetLogicTable(this IExpression expression)
+        {
+            if (!expression.IsLogicExpression())
+                throw new ExpressionEngineException("Only logical expressions");
+
+            if (expression.Variables == null)
+                throw new ExpressionEngineException();
+
+            var varialbes = expression.Flatten().OfType<Variable>().ToArray();
+            checked
+            {
+                int combinations = 1 << varialbes.Length;
+                for (int i=0; i<combinations; i++)
+                {
+                    bool[] values = GetValues(i, varialbes.Length);
+                    for (int j=0; j<values.Length; j++)
+                    {
+                        expression.Variables[varialbes[j].Name] = new NumberImplementation(values[j]);
+                    }
+                    var result = expression.Evaluate().GetBooean();
+                    if (result)
+                    {
+                        yield return i;
+                    }
+                }
+            }
+        }
+
+        private static bool[] GetValues(int currentIndex, int length)
+        {
+            bool[] result = new bool[length];
+            var str = Convert.ToString(currentIndex, 2).PadLeft(length, '0');
+            for (int j=0; j<str.Length; j++)
+            {
+                if (str[j] == '1')
+                    result[j] = true;
+                else
+                    result[j] = false;
+            }
+            return result;
+        }
     }
 }
---
+++ CalculatorShell.Tests/Expressions/ExtensionsTests.cs
@@ -0,0 +1,34 @@
+﻿using CalculatorShell.Expressions;
+using Moq;
+using NUnit.Framework;
+using System.Globalization;
+
+namespace CalculatorShell.Tests.Expressions
+{
+    [TestFixture]
+    public class ExtensionsTests
+    {
+        private Mock<IVariables> _variablesMock;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _variablesMock = new Mock<IVariables>();
+        }
+
+        [TestCase("a&b", true)]
+        [TestCase("a|b", true)]
+        [TestCase("!a&b", true)]
+        [TestCase("!(a&b)", true)]
+        [TestCase("(a&b)|c", true)]
+        [TestCase("a&true", true)]
+        [TestCase("a&false", true)]
+        [TestCase("1+2", false)]
+        public void TestIsLogicExpression(string expression, bool expected)
+        {
+            var parsed = ExpressionFactory.Parse(expression, _variablesMock.Object, CultureInfo.InvariantCulture);
+            var result = parsed.IsLogicExpression();
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
