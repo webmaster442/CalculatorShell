1ef713f9105bfe619bfe256b4120611779616ea5
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-07-10 17:07:05 +0200

    Basic stat implementation

---
+++ CalculatorShell.Math/StatisticResult.cs
@@ -0,0 +1,20 @@
+﻿namespace CalculatorShell.Maths
+{
+    public sealed record StatisticResult
+    {
+        public double Minimum { get; internal set; }
+        public double Maximum { get; internal set; }
+        public double Sum { get; internal set; }
+        public double Average => Sum / Count;
+        public double Count { get; internal set; }
+        public double Range => Maximum - Minimum;
+
+        public StatisticResult()
+        {
+            Minimum = double.NaN;
+            Maximum = double.NaN;
+            Sum = double.NaN;
+            Count = 0;
+        }
+    }
+}
---
+++ CalculatorShell.Math/Statistics.cs
@@ -0,0 +1,34 @@
+﻿namespace CalculatorShell.Maths
+{
+    public static class Statistics
+    {
+        public static StatisticResult Calculate(params double[] numbers)
+        {
+            StatisticResult ret = new();
+
+            if (numbers.Length < 1)
+                return ret;
+
+            ret.Maximum = numbers[0];
+            ret.Minimum = numbers[0];
+            ret.Sum = 0.0;
+            ret.Count = numbers.Length;
+
+            for (int i=0; i<numbers.Length; i++)
+            {
+                //Maximum
+                if (numbers[i] > ret.Maximum)
+                    ret.Maximum = numbers[i];
+
+                //Minimum
+                if (numbers[i] < ret.Minimum)
+                    ret.Minimum = numbers[i];
+
+                //Sum
+                ret.Sum += numbers[i];
+            }
+
+            return ret;
+        }
+    }
+}
