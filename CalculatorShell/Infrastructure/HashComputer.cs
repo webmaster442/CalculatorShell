16ab5556ae4ee55a5d1878d1364519f04b78242b
webmaster442 <ruzsinszki.gabor@gmail.com>
2021-08-26 17:46:29 +0200

    A hash computer

---
+++ CalculatorShell/Infrastructure/HashComputer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CalculatorShell.Infrastructure
+{
+    internal static class HashComputer
+    {
+        public static async Task<byte[]> ComputeHash(string file, HashAlgorithm algorithm, IProgress<double> progress, CancellationToken cancellationToken)
+        {
+            var buffer = new byte[1024 * 4];
+            int read = 0;
+            long processed = 0;
+            using (var fileStream = File.OpenRead(file))
+            {
+                long fileSize = fileStream.Length;
+                do
+                {
+                    read = await fileStream.ReadAsync(buffer, 0, buffer.Length);
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        break;
+                    }
+                    processed += read;
+
+                    if (read == 0)
+                    {
+                        algorithm.TransformFinalBlock(buffer, 0, read);
+                    }
+                    else
+                    {
+                        algorithm.TransformBlock(buffer, 0, read, buffer, 0);
+                    }
+
+                    progress.Report((double)processed / fileSize);
+                }
+                while (read > 0);
+            }
+
+            if (algorithm.Hash == null)
+                throw new InvalidOperationException();
+            else
+                return algorithm.Hash;
+        }
+    }
+}
